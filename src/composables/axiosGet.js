be5a499fd423c8625aa285263c3cabe4b33435fe
RD-png <ryan.543@live.co.uk>
2021-04-26 15:38:09 +0100

    Changed GET/POST urls to be only in axiosGet and axiosPost

--- src/composables/axiosGet.js
+++ src/composables/axiosGet.js
@@ -4,7 +4,7 @@ export function axiosGet(type) {
   // eslint-disable-next-line prettier/prettier
   const promise = axios.get(
     // URL below is for live system, change url when testing
-    `http://localhost/Ryan/Projects/stocksystem/PHPAPI/QueryController.php?${type}`
+    `http://192.168.0.24:8080/stocksystem/PHPAPI/QueryController.php?${type}`
   );
 
   const dataPromise = promise.then((response) => response.data);
--- src/composables/axiosPost.js
+++ src/composables/axiosPost.js
@@ -4,7 +4,7 @@ export function axiosPost(request, config = false) {
   try {
     axios.post(
       // URL below is for live system, change url when testing
-      "http://localhost/Ryan/Projects/stocksystem/PHPAPI/StockPost.php",
+      // "http://192.168.0.24:8080/stocksystem/PHPAPI/StockPost.php",
       request,
       config === false ? null : config
     );
