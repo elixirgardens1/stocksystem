acb9b2722699110f8901ad99f12c908d2dbd2a1e
RD-png <ryan.543@live.co.uk>
2021-04-26 15:44:48 +0100

    test exclude file

---
+++ src/composables/axiosGet.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export function axiosGet(type) {
+  // eslint-disable-next-line prettier/prettier
+  const promise = axios.get(
+    // URL below is for live system, change url when testing
+    // `http://localhost/Ryan/Projects/stocksystem/PHPAPI/QueryController.php?${type}`
+    `http://192.168.0.24:8080/stocksystem/PHPAPI/QueryController.php?${type}`
+  );
+
+  const dataPromise = promise.then((response) => response.data);
+
+  return dataPromise;
+}
